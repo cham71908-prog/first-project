24fc72b5d07510420e410292c682d853d46e291e
Hicham <cham71908@gmail.com>
2025-11-15 02:33:22 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Data types
+/*
+console Methods
+-log
+-error
+-table
+
+Web API
+
+Styling Console
+-Directive %c
+*/
+
+
+window.alert("no body is inpeccable");
+console.log("all is works");
+console.error("Error");
+console.table(["hicham", "taha", "achddir"]);
+
+console.log("hello %chicham %cachddir", "color : red; font - size: 10px", "color: green; font - size: 10px");
+
