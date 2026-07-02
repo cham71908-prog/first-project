75f5a74716d645205b1b325e813fec8e1fb46a9b
hicham <cham71908@gmail.com>
2026-07-02 20:53:12 +0200

    add switch

--- index.js
+++ index.js
@@ -253,6 +253,24 @@ let result = golls > 18? 'hello user'
 :'no you are very young'
 console.log(result)
 
+// Switch
+let role = prompt('what is your role')
+switch(role)
+{
+    case 'admin':
+        document.write('create , update , deleted');
+        break;
+    case 'moderator':
+            document.write('create , update');
+        break;
+    case 'editor':
+        document.write('update');
+        break;
+    default:
+        document.write('hello user');
+
+}
+
 
 
 
