ada8d35be61abead374998799689c87da22e8ef1
hicham <cham71908@gmail.com>
2026-09-08 15:53:26 +0200

    all js

--- fourth.js
+++ fourth.js
@@ -1,5 +1,33 @@
-function starClock() { }
+let secondsElapsed = 0;
+let interval = null;
+const time = document.getElementById("time")
 
-function stopClock() {}
+function padStart(value) {
+    return String(value).padStart(2, "0")
+}
 
-function resetClock() {}
+function setTime() {
+    const minutes = Math.floor(secondsElapsed / 60)
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}` ;
+}
+
+function timer() {
+    secondsElapsed++;
+    setTime()
+}
+
+function startClock() {
+    if (interval) stopClock()
+    interval = setInterval(timer, 1000)
+}
+
+function stopClock() {
+    clearInterval(interval)
+}
+
+function resetClock() {
+    stopClock()
+    secondsElapsed = 0;
+    setTime()
+}
